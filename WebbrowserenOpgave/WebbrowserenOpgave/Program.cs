4a84cc0ff7c92299bbb7595b2f1849fe3b0b1f6a
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-08-09 13:23:15 +0200

    Add files via upload

---
+++ WebbrowserenOpgave/WebbrowserenOpgave/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebbrowserenOpgave
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient();
+
+        public static async Task Main(string[] args)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://www.google.com");
+            response.EnsureSuccessStatusCode();
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            string formattedResponseStr = Regex.Replace(responseString, "<[^>]*>", String.Empty);
+            Console.WriteLine(formattedResponseStr);
+
+            Console.ReadLine();
+        }
+    }
+}
